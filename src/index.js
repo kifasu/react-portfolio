e977922a888f588b119430ab2d7b2f39ced73509
Aqmal Mansor <kifasu@gmail.com>
2021-03-19 01:09:33 +0800

    fixed react router

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ import Footer from './components/Footer';
 
 function App(){
     return (
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
             <div>
                 <Navbar/>
                 <div className="main">
