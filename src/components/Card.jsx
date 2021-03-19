5624344b81bbde434e5c024bc409cdefc6e51a15
Aqmal Mansor <kifasu@gmail.com>
2021-03-20 01:05:25 +0800

    fixed few features

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,9 @@ function Card(props) {
     return (
         <div className="card">
             <div className="card-title">
+                <a  className="card-link-title" href={props.desc} rel="noreferrer" target="_blank" >
                 <h1>{props.title}</h1>
+                </a>
             </div>
             <div className="card-link">
                 <a  className="card-link-a" href={props.desc} rel="noreferrer" target="_blank" >
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -22,9 +22,9 @@ function Contact() {
             <div className="about">
                 <div className="about-line"><h1>about me</h1></div><br/>
                 <h3 className="abt-content">Hi, I am Aqmal. </h3><br/>
-                <p className="abt-p">I'm a graduate of the local university in Malaysia. Being a science major, I know how to do most biotech-related stuff like gene extraction, statistical data analysis, plant pathology lab work and some on pineapple optimisation. Though I understand these kinds of things at a surface-level and by no necessary means an expert yet, I'm pretty confident in my skills to learn and get the job done. 
+                <p className="abt-p">I'm a graduate of the local university in Malaysia. Being a science major, I know how to do most biotech-related stuff like gene extraction, statistical data analysis, and some plant pathology lab work . Though I understand these kinds of things at a surface-level and by no necessary means an expert yet, I'm pretty confident in my skills to learn and get the job done. 
 <br/><br/>
-Recently, I found a book that changed the way I have been learning things. TLDR; that book has fair points on why I don't need to limit myself to one thing. What you know today do not necessarily determine what you want to be in the future. Sounds dramatic but it's true. Since then, I get excited about learning new technologies and do what I enjoy the best.
+Recently, I found a book that changed the way I have been learning things. TLDR; that book has fair points on why I don't need to limit myself to one thing. What you know today do not necessarily determine what you want pursue in the future. Sounds dramatic but it's true. Since then, I get excited about learning new technologies and do what I enjoy the best.
 </p>
             </div>
             <div className="contact">
@@ -32,7 +32,7 @@ Recently, I found a book that changed the way I have been learning things. TLDR;
                 <h4 className="ctc-content">
                 Feel free to hit me up if you are interested in working together for any cool project by 
                 sending me an email or whatsapp. Click on the links if you are lazy like me &#128516; <br/><br/> </h4>
-                <p><br/>Expect my replies within 96/2 hours. Under the circumstances you do not get an immediate answer from me, I might be in the middle of nowhere, but I promise you I will get back to you asap </p>
+                <p><br/>Expect my replies within 48 hours. Under the circumstances you do not get an immediate answer from me, I might be in the middle of nowhere, but I promise you I will get back to you asap </p>
                 <p><br/><br/>
                 <div className="lol">
                     <p>
